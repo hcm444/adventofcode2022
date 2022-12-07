9169480cbc2d171b2ac283c98b66db1830ba3006
Jacobus Burger <therealjacoburger@gmail.com>
2022-12-06 18:20:31 -0800

    added more explicit solutions

--- day1.py
+++ day1.py
@@ -1,6 +1,7 @@
 # Jacobus Burger (2022)
 
 
+# the solutions I used
 class Solution:
     def part1(file="day1.txt"):
         # parse the items into a nested list of inventories
@@ -21,5 +22,36 @@ class Solution:
             sum(map(int, items.strip().split('\n')))
             for items in open(file, 'r').read().split("\n\n")
         ]
+        calories.sort(reverse=True)
+        return sum(calories[:3])
+
+
+# less efficient in practice but maybe easier to read and understand
+class ReadableSolution:
+    def parse(file="day1.txt"):
+        inventories = []
+        with open(file, 'r') as f:
+            for inventory in f.read().split("\n\n"):
+                inventory = [
+                    int(item)
+                    for item in inventory.strip().split('\n')
+                ]
+                inventories.append(inventory)
+        return inventories
+
+
+    def part1(file="day1.txt"):
+        inventories = ReadableSolutions.parse(file)
+        calories = 0
+        for inventory in inventories:
+            inventory_calories = sum(inventory)
+            if inventory_calories >= calories:
+                calories = inventory_calories
+        return calories
+
+
+    def part2(file="day1.txt"):
+        inventories = ReadableSolutions.parse(file)
+        calories = [sum(inventory) for inventory in inventories]
         calories.sort(reverse=True)
         return sum(calories[:3])
