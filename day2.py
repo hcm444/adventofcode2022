db71713c7b07d66393fb246f65e9a96fc3fbd2b7
Jacobus Burger <therealjacoburger@gmail.com>
2022-12-23 12:18:39 -0800

    finished day 2 part 1 and added doc string

--- day2.py
+++ day2.py
@@ -1,7 +1,49 @@
 # Jacobus Burger (2022)
 
 
+
 class Solution:
+    def __doc__(self):
+        return """
+        # Advent of code, day 2 solutions.
+
+        I realized that I could map each move (rock, paper, scissors) to an
+        accompanying value (1, 2, 3) and then use some basic arithmetic to
+        determine the score.
+
+
+
+        ## Part 1
+
+        ### Explanation
+
+        We win when the opponent move is "behind" our move by 1 or by 2. In
+        principle, the moves Rock, Paper, and Scissors can be seen as a cyclic
+        directed graph with R < P < S < R as the relation between each move.
+        Arithmetically, if opponent chooses Rock (1), then subtracting that
+        from the winning move Paper (2) would result in a 1. This follows along
+        the entire chain except when they play Scissors (3) and we play
+        rock (1) in which case we get -2 instead.
+
+        As a consequence, the three states (win, tie, lose) are determined
+        by if `opponent - shape is -1 or 2`, `opponent is shape`, or
+        if `opponent - shape is 1 or -2`.
+
+        ### Pseudocode
+
+        ```
+        foreach round
+            opponent = ascii value of opponent move offset by 'A'
+            shape = ascii value of our move offset by 'X'
+
+            if opponent - shape is -1 or 2 then
+                score = shape + 6
+            if opponent is shape then
+                score = shape + 3
+            if opponent - shape is 1 or -2 then
+                score = shape + 0
+        ```
+    """
     def parse(self, filename):
         with open(filename, 'r') as f:
             strategies = [
@@ -11,29 +53,28 @@ class Solution:
             return strategies
 
 
-    def score(self, a, x):
-        # calculate shape score
-        shape = ord(x) - ord('X') + 1
-        opponent = ord(a) - ord('A') + 1
-        
-        # score according to play
-        # rock (1), paper (2), scissors (3)
-        # 1 wins 2 wins 3 wins 1
-        if opponent - shape == 1 or (opponent == 3 and shape == 1):
-            return shape + 6
-        if shape == opponent:
-            return shape + 3
-        if opponent - shape == -1 or (opponent == 1 and shape == 3):
-            return shape
-
-
-    def part1(self, filename):
+    def part1(self, filename="day2.txt"):
         strategies = self.parse(filename)
         total_score = 0
         for strategy in strategies:
-            total_score += self.score(strategy[0], strategy[1])
+            # get values for shapes (A = 1, B = 2, C = 3. X = 1, Y = 2, Z = 3)
+            opponent = ord(strategy[0]) - ord('A') + 1
+            shape = ord(strategy[1]) - ord('X') + 1
+
+            # rock wins paper wins scissors wins rock
+            if opponent - shape == -1 or opponent - shape == 2:
+                total_score += shape + 6  # win
+            if opponent == shape:
+                total_score += shape + 3  # tie
+            if opponent - shape == 1 or opponent - shape == -2:
+                total_score += shape + 0  # lose
         return total_score
 
 
-    def part2(self, filename):
+    def part2(self, filename="day2.txt"):
         pass
+
+
+day2 = Solution()
+solutions = [day2.part1(), day2.part2()]
+print(solutions)
