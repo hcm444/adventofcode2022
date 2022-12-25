1f88499ac5503b5d639e35b202c8465bbe9a09e5
Jacobus Burger <therealjacoburger@gmail.com>
2022-12-25 00:29:56 -0800

    commit work for today

--- day4.py
+++ day4.py
@@ -3,22 +3,33 @@
 
 class Solution:
     def parse(self, filename):
-        pass
+        # break into an array of ints (each pair of 4 is one line)
+        # eg: 2-4,6-8 => 2 4 6 8
+        with open(filename, 'r') as file:
+            assignments = [
+                int(num)
+                for line in file.readlines()
+                for pair in line.strip('\n').split(',')
+                for num in pair.split('-')
+            ]
+            return assignments
 
 
-    def part1(self, filename="day3.txt"):
+    def part1(self, filename="day4.txt"):
         # the most straightforward approach is to create a range
         #   and then check for membership of all elements of one
         #   in the other.
-        assignments = parse(filename)
-        for assignment in assignments:
+        assignments = self.parse(filename)
+        total = 0
+        for i in range(0, len(assignments), 4):
+            start_a, end_a, start_b, end_b = assignments[i:i+4]
             A = range(start_a, end_a)
             B = range(start_b, end_b)
             total += all(a in B for a in A) or all(b in A for b in B)
         return total
 
 
-    def part2(self, filename="day3.txt"):
+    def part2(self, filename="day4.txt"):
         pass
 
 
@@ -47,11 +58,9 @@ print(solutions)
 
 
 
+# for show, not for use
 class Alternative:
     def part1_logical(self, filename):
-        
-
-    def part1_alt(self, filename="day3.txt"):
         # the alternative solution is mathematical range checking
         #   this can be expressed as a one-liner logical expression,
         #   but it's easier to read this way
