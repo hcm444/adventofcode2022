11fa573fdd791b2b5e0b6c31ffd4aa245e90f9f2
Jacobus Burger <therealjacoburger@gmail.com>
2022-12-30 14:33:05 -0800

    finished advent of code, may come back soon

--- day1.py
+++ day1.py
@@ -16,54 +16,23 @@ class Solution:
 
 
     def part1(self, file="day1.txt"):
-        # parse the items into a nested list of inventories
-        # sum them together
-        # then choose the greatest of all the groups with max
+        # return the max calories
         return max(
             [
-                sum(map(int, items.strip().split('\n')))
-                for items in open(file, 'r').read().split("\n\n")
+                # sum all calories in each inventory
+                sum(map(int, inventory.strip().split('\n')))
+                # parse inventories
+                for inventory in open(file, 'r').read().split("\n\n")
             ]
         )
 
 
     def part2(self, file="day1.txt"):
-        # parse the items into a nexted list of inventories
-        # then we take a slice of the biggest 3 and get the total
+        # parse the inventories into a nexted list of inventories
         calories = [
-            sum(map(int, items.strip().split('\n')))
-            for items in open(file, 'r').read().split("\n\n")
+            sum(map(int, inventories.strip().split('\n')))
+            for inventories in open(file, 'r').read().split("\n\n")
         ]
-        calories.sort(reverse=True)
-        return sum(calories[:3])
-
-
-# less efficient in practice but maybe easier to read and understand
-class ReadableSolution:
-    def parse(self, file="day1.txt"):
-        inventories = []
-        with open(file, 'r') as f:
-            for inventory in f.read().split("\n\n"):
-                inventory = [
-                    int(item)
-                    for item in inventory.strip().split('\n')
-                ]
-                inventories.append(inventory)
-        return inventories
-
-
-    def part1(self, file="day1.txt"):
-        inventories = ReadableSolutions.parse(file)
-        calories = 0
-        for inventory in inventories:
-            inventory_calories = sum(inventory)
-            if inventory_calories >= calories:
-                calories = inventory_calories
-        return calories
-
-
-    def part2(self, file="day1.txt"):
-        inventories = ReadableSolutions.parse(file)
-        calories = [sum(inventory) for inventory in inventories]
+        # then we take a slice of the biggest 3 and get the total
         calories.sort(reverse=True)
         return sum(calories[:3])
