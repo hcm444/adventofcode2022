2284f80a914144b746099ed26e2b1ea16a1610b7
Jacobus Burger <therealjacoburger@gmail.com>
2023-01-06 18:52:23 -0800

    improve day1 documentation

--- day1.py
+++ day1.py
@@ -16,23 +16,22 @@ class Solution:
 
 
     def part1(self, file="day1.txt"):
-        # return the max calories
-        return max(
-            [
-                # sum all calories in each inventory
-                sum(map(int, inventory.strip().split('\n')))
-                # parse inventories
-                for inventory in open(file, 'r').read().split("\n\n")
-            ]
-        )
+        # record total calories of each elf inventory
+        calories = [
+            sum(map(int, inventory.strip().split('\n')))
+            for inventory in open(file, 'r').read().split("\n\n")
+        ]
+        # find the maximum
+        return max(calories)
 
 
     def part2(self, file="day1.txt"):
-        # parse the inventories into a nexted list of inventories
+        # record total calories of each elf inventory
         calories = [
             sum(map(int, inventories.strip().split('\n')))
             for inventories in open(file, 'r').read().split("\n\n")
         ]
-        # then we take a slice of the biggest 3 and get the total
+        # reverse sort calores (first 3 are top 3)
         calories.sort(reverse=True)
+        # take the first 3 and return their sum
         return sum(calories[:3])
