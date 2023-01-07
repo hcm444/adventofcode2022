98f184d43c7f4b108eb0e23b9709791d074ab0a4
Henry Meier <32826270+hcm444@users.noreply.github.com>
2023-01-07 00:58:26 -0800

    Update day3.py
    Reduced memory
    
    avoid re creating a set object

--- day3.py
+++ day3.py
@@ -1,74 +1,32 @@
-# Jacobus Burger (2022)
+from collections import defaultdict
 from functools import reduce
+from itertools import islice
 from string import ascii_letters
 
-
 class Solution:
-    def __doc__(self):
-        return """
-        # Day 3
-
-        ## Part 1
-
-        ### Explanation
-
-        Given a sack (string), I need to split it into two
-        compartments (strings) and determine what item (character)
-        they have in common, then get the priority value of that
-        item, and add that together to get the total priority of all.
-
-        To do this I just wrote a function to calculate the correct
-        priority levels (a-z == 1-26, A-Z == 27-52). Then, iterating
-        through every string, I use python's set intersection to find
-        the common item, use the priority function to calculate the
-        value, and then add it to a running total then return that total.
-
-        ## Part 2
-
-        ### Explanation
-
-        To solve this, I take groups of 3 sacks at a time and
-        find the intersection between all three of them with
-        a reduce. Then add the priority value of the answer to the
-        running total and return that running total.
-    """
-
-
     def parse(self, filename="day3.txt"):
         with open(filename, 'r') as file:
-            return [
-                sack.strip('\n')
-                for sack in file.readlines()
-            ]
-
+            while True:
+                lines = list(islice(file, 1000))
+                if not lines:
+                    break
+                yield ''.join(lines).strip()
 
     def part1(self, filename="day3.txt"):
-        sacks = self.parse(filename)
-        total = 0
-        for sack in sacks:
-            # find common character (item type) in both compartments
+        counts = defaultdict(int)
+        for sack in self.parse(filename):
             A = set(sack[:len(sack) // 2])  # compartment 1
             B = set(sack[len(sack) // 2:])  # compartment 2
-            item = A.intersection(B).pop()  # common item
-
-            # add common item type priority to total
-            total += ascii_letters.index(item) + 1
-        return total
-
+            item = next(iter(A.intersection(B)))  # common item
+            counts[item] += 1
+        return sum(counts[item] * (ascii_letters.index(item) + 1) for item in counts)
 
     def part2(self, filename="day3.txt"):
-        sacks = self.parse(filename)
-        total = 0
-        # iterate 3 sacks at a time (one group at a time)
-        for i in range(0, len(sacks), 3):
-            group = sacks[i:i + 3]
-            badge = reduce(set.intersection, map(set, group)).pop()
-            total += ascii_letters.index(badge) + 1
-        return total
-
-
-
-
+        counts = defaultdict(int)
+        for group in zip(*(self.parse(filename),) * 3):
+            badge = next(iter(reduce(set.intersection, map(set, group))))
+            counts[badge] += 1
+        return sum(counts[item] * (ascii_letters.index(item) + 1) for item in counts)
 
 day3 = Solution()
 solutions = [day3.part1(), day3.part2()]
